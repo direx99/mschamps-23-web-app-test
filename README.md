fb0e45166af8f414793f38ce65d82ba17f130d63
Nikila99gimhan <nikila99fernando.gimhan@gmail.com>
2023-03-25 00:16:59 +0530

    init_commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# mschamps-23-web-app-test
-This web app is for testing purposes only.
--- public/script.js
+++ public/script.js
@@ -1,10 +1,26 @@
-const form = document.querySelector("#my-form");
-const nameInput = document.querySelector("#name");
-const emailInput = document.querySelector("#email");
-const outputDiv = document.querySelector("#output");
+document.addEventListener("DOMContentLoaded", () => {
+  const dateElement = document.getElementById("date");
+  const timeElement = document.getElementById("time");
+  const currentDate = new Date();
+  const formattedDate = currentDate.toLocaleDateString(undefined, {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const name = nameInput.value;
-  outputDiv.innerHTML = `Hello, ${name}! It's nice to meet you.<br />Thanks for visiting my web app.`;
+  dateElement.textContent = formattedDate;
+
+  function updateTime() {
+    const currentTime = new Date();
+    const formattedTime = currentTime.toLocaleTimeString(undefined, {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    });
+    timeElement.textContent = formattedTime;
+  }
+
+  updateTime();
+  setInterval(updateTime, 1000);
 });
